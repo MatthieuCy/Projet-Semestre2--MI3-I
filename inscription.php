4f42041ae2b0c33795f0ad28477e1d6d025d6263
Ibrahim75cy <ibrahim.fridjine@gmail.com>
2026-04-04 16:24:09 +0200

    changement chemin absolu a relatif pour le css

--- inscription.php
+++ inscription.php
@@ -58,7 +58,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Inscription - Pizza Nova</title>
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <?php $base = '';
